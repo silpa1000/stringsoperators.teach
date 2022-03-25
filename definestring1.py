6d511eebd37e497c8fe27a474168701b55d719ad
silpa1000 <100128518+silpa1000@users.noreply.github.com>
2022-03-25 12:20:14 +0530

    Add files via upload
    #defining a string

---
+++ definestring1.py
@@ -0,0 +1,6 @@
+x="Python is a programming language that lets you work quickly and integrate systems more effectively"
+print(x.upper())
+print(x.lower())
+print(x.replace('a','*'))
+if 'you' in x:
+    print('FOUND')
